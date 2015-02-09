b4f86657ce4b1ae6c685bf97af61180c0f1fef8b
Daniella Shin <thedanpan@gmail.com>
2015-02-09 00:27:42 -0500

    create eventlistener to recognize keyup in document

--- app/assets/javascripts/markdown.js
+++ app/assets/javascripts/markdown.js
@@ -1,3 +1,9 @@
+function GetKeyUp() {
+  $("textarea").keyup(function(e) {
+    awesomeWidget.convertText(e);
+  })
+};
+
 $(document).ready(function() {
   awesomeWidget = new MarkdownWidget("#source-id", "#preview-div");
   GetKeyUp();

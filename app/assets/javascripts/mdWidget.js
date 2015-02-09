95d3754ec3e75e95b8308b7f241bed984e80f4de
Daniella Shin <thedanpan@gmail.com>
2015-02-09 00:25:26 -0500

    add markdown prototype method to display text

--- app/assets/javascripts/mdWidget.js
+++ app/assets/javascripts/mdWidget.js
@@ -1,4 +1,11 @@
 function MarkdownWidget(source, preview) {
   this.source = source;
   this.preview = preview;
+  this.newText = "AWESOME TEXT";
+}
+
+MarkdownWidget.prototype.convertText = function(e) {
+  var sourceText = $(this.source).val();
+  this.newText = sourceText;
+  $(this.preview).html(this.newText);
 }

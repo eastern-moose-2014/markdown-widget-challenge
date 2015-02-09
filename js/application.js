17c2d9a20eda0cb169b3b7d6e347afe1793e0d08
Amar Singh <ummerr@gmail.com>
2015-02-08 20:16:49 -0500

    getting value of text area

--- js/application.js
+++ js/application.js
@@ -1,7 +1,12 @@
 $(document).ready(function() {
 
-  $('#source').keyup(function() {
-    //this is the controller
+  var $source = $('#source')
+
+  $source.keyup(function() {
+    var x = $('#source-text').val();
+    console.log(x);
+    // var text =
+    // mkd.getText(text)
   });
 
 });
@@ -14,12 +19,16 @@ View.updateRendered = function (argument) {
   $("#preview").append("argument")
 }
 
+
+//Below is the model
+
 function MarkdownWidget() {
-  this.text = ""
+  this.text = ''
 }
 
-MarkdownWidget.prototype.getText = function(){
-  return //text from source
+
+MarkdownWidget.prototype.getText = function(string){
+  this.text = string
 }
 
 MarkdownWidget.prototype.convertText = function(){

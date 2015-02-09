0e6f9a9a124a80521ac79043be702df1cb7aef72
Amar Singh <ummerr@gmail.com>
2015-02-08 20:57:32 -0500

    iterating over the array is WIP

--- js/application.js
+++ js/application.js
@@ -1,12 +1,15 @@
 $(document).ready(function() {
 
+  mkd = new MarkdownWidget();
+
   var $source = $('#source')
 
   $source.keyup(function() {
-    var x = $('#source-text').val();
-    console.log(x);
-    // var text =
-    // mkd.getText(text)
+    var sourceText = $('#source-text').val();
+    mkd.getText(sourceText);
+    mkd.convertText(mkd.splitText());
+    //convert the source to html to render
+    //View.updateRendered(mkd.finalOutput)
   });
 
 });
@@ -23,16 +26,26 @@ View.updateRendered = function (argument) {
 //Below is the model
 
 function MarkdownWidget() {
-  this.text = ''
+  this.text = ""
 }
-
 
 MarkdownWidget.prototype.getText = function(string){
   this.text = string
 }
 
-MarkdownWidget.prototype.convertText = function(){
-   //text from getText check against conversion conditions
+MarkdownWidget.prototype.splitText = function(string) {
+  return this.text.split("");
+}
+
+MarkdownWidget.prototype.convertText = function(textArray){
+  var join = textArray.forEach(function(letter) {
+    if (letter === '*') {
+      console.log("Found one!")
+    }
+    else if (letter === '_') {
+      console.log("found another one!")
+    }
+  })
 }
 
 MarkdownWidget.prototype.updateView = function() {

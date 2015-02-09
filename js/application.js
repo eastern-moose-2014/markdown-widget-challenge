479a4e02280d65b04c8f9bb88729bbbfcd9357fa
Amar Singh <ummerr@gmail.com>
2015-02-08 19:55:27 -0500

    added pseudo code

--- js/application.js
+++ js/application.js
@@ -1,8 +1,7 @@
-
 $(document).ready(function() {
 
   $('#source').keyup(function() {
-    $("#preview").append("what")
+    //this is the controller
   });
 
 });
@@ -12,6 +11,23 @@ var View = {};
 
 View.updateRendered = function (argument) {
 // Out put of the markdown goes here.
+  $("#preview").append("argument")
+}
+
+function MarkdownWidget() {
+  this.text = ""
+}
+
+MarkdownWidget.prototype.getText = function(){
+  return //text from source
+}
+
+MarkdownWidget.prototype.convertText = function(){
+   //text from getText check against conversion conditions
+}
+
+MarkdownWidget.prototype.updateView = function() {
+  //take converted text in HTML form and update the view
 }
 
 

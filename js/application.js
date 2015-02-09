4184b39ba432c4ed1758514293668b6f3be10c1c
Amar Singh <ummerr@gmail.com>
2015-02-08 19:20:26 -0500

    key up function is working with a console test

--- js/application.js
+++ js/application.js
@@ -1,3 +1,12 @@
 function hello () {
   console.log("hello world")
 }
+
+$(document).ready(function() {
+
+  // Mousetrap.bind('keyup')
+  $('#source').keyup(function() {
+    console.log("key up working")
+  });
+
+});

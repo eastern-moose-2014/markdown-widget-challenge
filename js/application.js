fc1553fd2ec97a81e881ac0e5ba1d2b22d285c09
Amar Singh <ummerr@gmail.com>
2015-02-08 19:43:37 -0500

    added a appending of text when keydown pressed in down

--- js/application.js
+++ js/application.js
@@ -1,12 +1,17 @@
-function hello () {
-  console.log("hello world")
-}
 
 $(document).ready(function() {
 
-  // Mousetrap.bind('keyup')
   $('#source').keyup(function() {
-    console.log("key up working")
+    $("#preview").append("what")
   });
 
 });
+
+
+var View = {};
+
+View.updateRendered = function (argument) {
+// Out put of the markdown goes here.
+}
+
+

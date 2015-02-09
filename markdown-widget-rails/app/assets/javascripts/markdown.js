3c7016912391b0045c9fb05496cd3e7f6b86cfbc
Dionne Stanfield <dstanfield.esq@gmail.com>
2015-02-08 20:32:18 -0500

    append keystroke events to tiv to show text insertion

--- markdown-widget-rails/app/assets/javascripts/markdown.js
+++ markdown-widget-rails/app/assets/javascripts/markdown.js
@@ -1,3 +1,14 @@
 $(document).ready(function() {
 
+  $target = $('#text-box');
+  $output = $('#output-div');
+
+  $target.keyup(function(event) {
+    var typedChar = String.fromCharCode(event.keyCode);
+    console.log(typedChar);
+
+    $output.children().last().append(typedChar);
+
+  });
+
 });
